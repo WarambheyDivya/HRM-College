e7da92cde53a2f3af1b7304f3f2c4a473dd701a2
KumarShubham001 <shubhamrock6567@gmail.com>
2018-01-16 17:36:32 +0530

    many pages, cant think of

--- tool/3.php
+++ tool/3.php
@@ -35,31 +35,36 @@ include 'footer.php';
                 <h2 style=" font-weight: bold;">Experience Details</h2>
                 <form style="padding-top: 6%;" action="persnol.php" method="post">
                     <div class="form-group row">
-                        <label for="inputQualification1" class="col-sm-2 col-form-label">Teaching Exp.</label>
+                        <label for="inputTeaching" class="col-sm-2 col-form-label">Teaching Exp.</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" name="qualification1" id="inputQualification1" placeholder="Qualification">
+                            <input type="text" class="form-control" name="teaching_ex" id="inputTeaching" placeholder="Teaching exp.">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="inputQualification2" class="col-sm-2 col-form-label">Industrial Exp.</label>
+                        <label for="inputIndustrial" class="col-sm-2 col-form-label">Industrial Exp.</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" name="qualification2" id="inputQualification2" placeholder="Qualification">
+                            <input type="text" class="form-control" name="industrial_ex" id="inputIndustrial" placeholder="Industrial exp.">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="inputQualification3" class="col-sm-2 col-form-label">Research Exp.</label>
+                        <label for="inputResearch" class="col-sm-2 col-form-label">Research Exp.</label>
                         <div class="col-sm-10">
-                            <input type="text" class="form-control" name="Qualification3" id="inputQualification3" placeholder="Qualification">
+                            <input type="text" class="form-control" name="research_ex" id="inputResearch" placeholder="Research exp.">
                         </div>
                     </div>
                     <div class="form-group row">
-                        <label for="inputPhd" class="col-sm-2 col-form-label">PHD Status</label>
+                        <label for="inputExam1" class="col-sm-2 col-form-label">Exp. as univsersity exam evaluator</label>
                         <div class="col-sm-10">
-                            <select name="php" id="inputPhd" class="form-control">
-                                <option>Completed</option>
-                                <option>Pursuing</option>
-                                <option selected>No PHD</option>
+                            <select id="inputExam1" class="form-control">
+                                <option value="yes">Yes</option>
+                                <option value="no" selected>No</option>
                             </select>
+                        </div>
+                    </div>
+                    <div class="form-group row" id="hidden" style="display: block;">
+                        <label for="inputPhd" class="col-sm-2 col-form-label">Exp. as univsersity exam evaluator</label>
+                        <div class="col-sm-10">
+                            <input type="text" placeholder="Years" id="inputExam2" name="exam_ex" class="form-control">
                         </div>
                     </div>
                     <div class="form-group row">
